6c1cc75ac42952e41a7a555174859a4b44d685de
Lev <levfish@hotmail.com>
2017-12-03 23:02:51 +0200

    Add Runner Class

---
+++ src/main/java/com/lev/Runner.java
@@ -0,0 +1,10 @@
+package com.lev;
+
+/**
+ * Created by Lev on 12/3/2017.
+ */
+    public class Runner {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+}
